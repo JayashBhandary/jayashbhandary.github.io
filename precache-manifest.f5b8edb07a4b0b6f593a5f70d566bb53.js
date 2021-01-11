009040d14aafc39933d904a4dc2c3ab08ada5d3c
Jayash Bhandary <70916302+JayashBhandary@users.noreply.github.com>
2021-01-11 17:29:56 +0530

    First Commit

---
+++ precache-manifest.f5b8edb07a4b0b6f593a5f70d566bb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26cbb2a22f2f2095664f23264ee2f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e98d017371cc2605684f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98d017371cc2605684f",
+    "url": "/static/js/main.51ceccf9.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
